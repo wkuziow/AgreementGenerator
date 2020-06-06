7685d52d81819d1fb27b40c495e39c000b3e1677
Wojciech Kuziow <wkuziow@gmail.com>
2020-06-06 10:28:53 +0200

    first commit

---
+++ src/main/java/pl/coderslab/agreementgenerator/AgreementGeneratorApplication.java
@@ -0,0 +1,12 @@
+package pl.coderslab.agreementgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgreementGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgreementGeneratorApplication.class, args);
+    }
+}
