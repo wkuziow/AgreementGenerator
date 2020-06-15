97b8bc6f6cc94934f86caf4e25978b8771c634da
Wojciech Kuziow <wkuziow@gmail.com>
2020-06-15 17:08:13 +0200

    agreement page

--- src/main/java/pl/coderslab/agreementgenerator/transaction/TransactionRepository.java
+++
@@ -1,6 +0,0 @@
-package pl.coderslab.agreementgenerator.transaction;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
